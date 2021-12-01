d7b62b81eab93edf3f02d641e865f9c50378141e
Szczepan Ćwikliński <sec@thinkcode.pl>
2021-12-01 06:21:31 +0100

    Day 01

--- src/src/Day01.cs
+++ src/src/Day01.cs
@@ -4,11 +4,35 @@ internal class Day01 : BaseDay
 {
     protected override string Part1()
     {
-        return this.ReadAllText();
+        var input = this.ReadAllLines().Select(int.Parse).ToList();
+        var part1 = 0;
+        var prev = input.First();
+
+        foreach (var m in input)
+        {
+            part1 += m > prev ? 1 : 0;
+            prev = m;
+        }
+
+        return part1.ToString();
     }
 
     protected override string Part2()
     {
-        return this.ReadAllText();
+        var input = this.ReadAllLines().Select(int.Parse).ToList();
+        var part2 = 0;
+        var prev = input.First();
+
+        for (int i = 0; i < input.Count - 2; i++)
+        {
+            var sum = input.Skip(i).Take(3).Sum();
+            if (sum > prev)
+            {
+                part2++;
+            }
+            prev = sum;
+        }
+
+        return part2.ToString();
     }
 }
