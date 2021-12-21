410d0d962d417de1ab65f6f8e6f8e749f77cdfaf
Szczepan Ćwikliński <sec@thinkcode.pl>
2021-12-21 12:35:17 +0100

    Day 21 Part 2

--- src/src/Day21.cs
+++ src/src/Day21.cs
@@ -2,12 +2,16 @@
 
 internal class Day21 : BaseDay
 {
+    static readonly (int, int, int)[] _diracDiceRolls = new[] { (1, 1, 1), (1, 1, 2), (1, 1, 3), (1, 2, 1), (1, 2, 2), (1, 2, 3), (1, 3, 1), (1, 3, 2), (1, 3, 3), (2, 1, 1), (2, 1, 2), (2, 1, 3), (2, 2, 1), (2, 2, 2), (2, 2, 3), (2, 3, 1), (2, 3, 2), (2, 3, 3), (3, 1, 1), (3, 1, 2), (3, 1, 3), (3, 2, 1), (3, 2, 2), (3, 2, 3), (3, 3, 1), (3, 3, 2), (3, 3, 3) };
+    static readonly Dictionary<(int, int, int, int), (long, long)> _cache = new();
+
+    int[] StartPosition => ReadAllLinesSplit(" ").Select(x => int.Parse(x.Last()) - 1).ToArray();
+
     int Practice()
     {
-        var input = ReadAllLinesSplit(" ");
         var roll = 0;
         var scores = new int[2];
-        var position = input.Select(x => int.Parse(x.Last()) - 1).ToArray();
+        var position = StartPosition.ToArray();
 
         while (true)
         {
@@ -26,10 +30,43 @@ internal class Day21 : BaseDay
         }
     }
 
+    (long P1, long P2) PlayDice(int p1score, int p2score, int p1pos, int p2pos)
+    {
+        var cacheKey = (p1score, p2score, p1pos, p2pos);
+
+        if (_cache.TryGetValue(cacheKey, out var cache))
+        {
+            return cache;
+        }
+
+        if (p1score >= 21)
+        {
+            return (1, 0);
+        }
+
+        if (p2score >= 21)
+        {
+            return (0, 1);
+        }
+
+        var score1 = 0L;
+        var score2 = 0L;
+
+        foreach (var (a, b, c) in _diracDiceRolls)
+        {
+            var new_pos = (p1pos + a + b + c) % 10;
+            var new_score = p1score + new_pos + 1;
+
+            var (p2, p1) = PlayDice(p2score, new_score, p2pos, new_pos);
+
+            score1 += p1;
+            score2 += p2;
+        }
+
+        return _cache[cacheKey] = (score1, score2);
+    }
+
     protected override object Part1() => Practice();
 
-    protected override object Part2()
-    {
-        return -1;
-    }
+    protected override object Part2() => PlayDice(0, 0, StartPosition[0], StartPosition[1]).P1;
 }
