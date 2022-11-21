f7d2e53dce8111fbc24bcb9cf9feddccf4d86f62
Szczepan Ćwikliński <sec@thinkcode.pl>
2022-11-21 21:39:55 +0100

    Day 24 and Day 22 (unsolved part 2)

---
+++ src/src/Day22.cs
@@ -0,0 +1,150 @@
+﻿namespace aoc2021.src;
+
+internal class Day22 : BaseDay
+{
+    record struct Point(int X, int Y, int Z);
+
+    record struct Step(Point From, Point To, bool State)
+    {
+        public long SizeX => To.X - From.X + 1;
+        public long SizeY => To.Y - From.Y + 1;
+        public long SizeZ => To.Z - From.Z + 1;
+
+        public long Volume => SizeX * SizeY * SizeX;
+
+        public static bool Intersect(Step a, Step b)
+        {
+            return (a.From.X <= b.To.X && a.To.X >= b.From.X) && (a.From.Y <= b.To.Y && a.To.Y >= b.From.Y) && (a.From.Z <= b.To.Z && a.To.Z >= b.From.Z);
+        }
+    }
+
+    record struct Cuboid(Point Start, Point End)
+    {
+    }
+
+    IEnumerable<Step> ParseInput()
+    {
+        foreach (var step in ReadAllLinesSplit(" ").Select(i => new { State = i[0] == "on", Data = i[1].Split(',') }))
+        {
+            var x = step.Data[0][2..].Split("..").Select(int.Parse).ToArray();
+            var y = step.Data[1][2..].Split("..").Select(int.Parse).ToArray();
+            var z = step.Data[2][2..].Split("..").Select(int.Parse).ToArray();
+
+            yield return new Step(new Point(x[0], y[0], z[0]), new Point(x[1], y[1], z[1]), step.State);
+        }
+    }
+
+    protected override object Part1()
+    {
+        var cube = new Dictionary<Point, bool>();
+
+        foreach (var step in ParseInput().ToList())
+        {
+            if (step.From.X < -50 || step.From.X > 50 || step.From.Y < -50 || step.From.Y > 50 || step.From.Z < -50 || step.From.Z > 50)
+            {
+                continue;
+            }
+
+            for (var x = step.From.X; x <= step.To.X; x++)
+            {
+                for (var y = step.From.Y; y <= step.To.Y; y++)
+                {
+                    for (var z = step.From.Z; z <= step.To.Z; z++)
+                    {
+                        cube[new Point(x, y, z)] = step.State;
+                    }
+                }
+            }
+        }
+
+        return cube.Count(kv => kv.Value);
+    }
+
+    void PrintOpenScad()
+    {
+        var off = new StringBuilder();
+        var on = new StringBuilder();
+        foreach (var step in ParseInput().ToList())
+        {
+            var sb = step.State ? on : off;
+            sb.AppendLine($"translate([{step.From.X}, {step.From.Y}, {step.From.Z}]) cube([{step.SizeX}, {step.SizeY}, {step.SizeZ}]);");
+        }
+        Console.WriteLine($"difference() {{");
+        Console.WriteLine($"union() {{ {on} }};");
+        Console.WriteLine($"union() {{ {off} }};");
+        Console.WriteLine($"}}");
+    }
+
+    protected override object Part2()
+    {
+        var cubes = new List<Step>();
+        var volumes = new List<long>();
+
+        foreach (var step in ParseInput())
+        {
+            if (step.State)
+            {
+                // add new cube volume to list
+                volumes.Add(step.Volume);
+                // check for common parts with old cubes
+                foreach (var old in cubes)
+                {
+                    if (Step.Intersect(old, step))
+                    {
+                        var common = GetCommonVolume(step, old);
+                        volumes.Add(-common);
+                    }
+                }
+                // add new cube to cubes
+                cubes.Add(step);
+            }
+            else
+            {
+                foreach (var old in cubes)
+                {
+                    if (Step.Intersect(old, step))
+                    {
+                        var common = GetCommonVolume(step, old);
+                        volumes.Add(-common);
+                    }
+                }
+            }
+        }
+
+        return volumes.Sum();
+    }
+
+    static long GetCommonVolume(Step step, Step old)
+    {
+        //if (old.Volume < step.Volume)
+        //{
+        //    var tmp = step;
+        //    step = old;
+        //    old = tmp;
+        //}
+
+        var common = 0L;
+        Parallel.For(step.From.X, step.To.X + 1, x =>
+        //for (var x = step.From.X; x <= step.To.X; x++)
+        {
+              Parallel.For(step.From.Y, step.To.Y + 1, y =>
+              //for (var y = step.From.Y; y <= step.To.Y; y++)
+              {
+                  Parallel.For(step.From.Z, step.To.Z + 1, z =>
+                  //for (var z = step.From.Z; z <= step.To.Z; z++)
+                  {
+                      var cx = x >= old.From.X && x <= old.To.X;
+                      var cy = y >= old.From.Y && y <= old.To.Y;
+                      var cz = z >= old.From.Z && z <= old.To.Z;
+                      if (cx && cy && cz)
+                      {
+                          Interlocked.Increment(ref common);
+                          //common++;
+                      }
+                  });
+              });
+
+          });
+        return common;
+    }
+}
---
+++ src/src/Day24.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.Concurrent;
+
+namespace aoc2021.src;
+
+internal class Day24 : BaseDay
+{
+    long _min = 0;
+    long _max = 0;
+
+    record struct State(long[] Regs, long Min, long Max)
+    {
+        public (long, long, long, long) GetRegs() => (Regs[0], Regs[1], Regs[2], Regs[3]);
+    }
+
+    static int Index(string r)
+    {
+        return r switch
+        {
+            "x" => 0,
+            "y" => 1,
+            "w" => 2,
+            "z" => 3,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    static long Value(string r, long[] regs)
+    {
+        return r switch
+        {
+            "x" or "y" or "w" or "z" => regs[Index(r)],
+            _ => long.Parse(r)
+        };
+    }
+
+    protected override object Part1()
+    {
+        var state = new List<State>() { new State(new[] { 0L, 0, 0, 0 }, 0, 0) };
+
+        foreach (var line in ReadAllLines(true))
+        {
+            var data = line.Split(' ');
+            var op = data[0];
+            var left = Index(data[1]);
+            var right = data.Length == 3 ? data[2] : "";
+
+            switch (op)
+            {
+                case "inp":
+                    {
+                        // remove duplicate states
+                        var hash = new Dictionary<(long X, long Y, long W, long Z), (long Min, long Max)>();
+                        foreach (var s in state)
+                        {
+                            s.Regs[left] = 0;
+
+                            var regs = s.GetRegs();                            
+
+                            if (hash.TryGetValue(regs, out var val))
+                            {
+                                var min = Math.Min(s.Min, val.Min);
+                                var max = Math.Max(s.Max, val.Max);
+
+                                hash[regs] = (min, max);
+                            }
+                            else
+                            {
+                                hash[regs] = (s.Min, s.Max);
+                            }
+                        }
+
+                        // populate state without duplicates
+                        state.Clear();
+                        foreach (var kv in hash)
+                        {
+                            var (X, Y, W, Z) = kv.Key;
+                            var (Min, Max) = kv.Value;
+
+                            var item = new State(new[] { X, Y, W, Z }, Min, Max);
+
+                            state.Add(item);
+                        }
+
+                        Console.WriteLine($"State count: {state.Count}");
+
+                        // generate new states
+                        var next = new ConcurrentBag<State>();
+                        Parallel.ForEach(state, s =>
+                        {
+                            for (int i = 1; i <= 9; i++)
+                            {
+                                var regs = new[] { s.Regs[0], s.Regs[1], s.Regs[2], s.Regs[3] };
+                                regs[left] = i;
+
+                                var min = s.Min * 10 + i;
+                                var max = s.Max * 10 + i;
+
+                                next.Add(new(regs, min, max));
+                            }
+                        });
+                        state.Clear();
+                        state = next.ToList();
+                    }
+                    break;
+
+                case "add":
+                    {
+                        Parallel.ForEach(state, s =>
+                        {
+                            var a = s.Regs[left];
+                            var b = Value(right, s.Regs);
+
+                            s.Regs[left] = a + b;
+                        });
+                    }
+                    break;
+
+                case "mul":
+                    {
+                        Parallel.ForEach(state, s =>
+                        {
+                            var a = s.Regs[left];
+                            var b = Value(right, s.Regs);
+
+                            s.Regs[left] = a * b;
+                        });
+                    }
+                    break;
+
+                case "div":
+                    {
+                        Parallel.ForEach(state, s =>
+                        {
+                            var a = s.Regs[left];
+                            var b = Value(right, s.Regs);
+
+                            s.Regs[left] = a / b;
+                        });
+                    }
+                    break;
+
+                case "mod":
+                    {
+                        Parallel.ForEach(state, s =>
+                        {
+                            var a = s.Regs[left];
+                            var b = Value(right, s.Regs);
+
+                            s.Regs[left] = a % b;
+                        });
+                    }
+                    break;
+
+                case "eql":
+                    {
+                        Parallel.ForEach(state, s =>
+                        {
+                            var a = s.Regs[left];
+                            var b = Value(right, s.Regs);
+
+                            s.Regs[left] = (a == b) ? 1 : 0;
+                        });
+                    }
+                    break;
+            }
+        }
+
+        var zIndex = Index("z");
+        var final = state.Where(x => x.Regs[zIndex] == 0).ToList();
+
+        _max = final.MaxBy(x => x.Max).Max;
+        _min = final.MinBy(x => x.Min).Min;
+
+        return _max;
+    }
+
+    protected override object Part2() => _min;
+}
